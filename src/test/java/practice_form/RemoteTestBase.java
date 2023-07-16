60b06d14c7b8fb7cd3385cc77a329d6982ae3403
Marena <fabriana.sands@gmail.com>
2023-07-16 18:43:17 +0500

    add attach

--- src/test/java/practice_form/RemoteTestBase.java
+++ src/test/java/practice_form/RemoteTestBase.java
@@ -2,12 +2,17 @@ package practice_form;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import practice_form.helpers.Attach;
 
 import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class RemoteTestBase {
 
@@ -33,15 +38,14 @@ public class RemoteTestBase {
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
-//
-//    @AfterEach
-//    void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//
-//        closeWebDriver();
-//    }
-   // }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
+    }
 }
