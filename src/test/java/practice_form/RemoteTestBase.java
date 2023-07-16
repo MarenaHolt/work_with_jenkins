b21b0afd3fbe165815003ba54f36ce79c4cd6a6a
Marena <fabriana.sands@gmail.com>
2023-07-16 18:30:10 +0500

    add example

--- src/test/java/practice_form/RemoteTestBase.java
+++ src/test/java/practice_form/RemoteTestBase.java
@@ -1,7 +1,10 @@
 package practice_form;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -26,10 +29,10 @@ public class RemoteTestBase {
         Configuration.browserCapabilities = capabilities;
     }
 
-//    @BeforeEach
-//    void addListener() {
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-//    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 //
 //    @AfterEach
 //    void addAttachments() {
